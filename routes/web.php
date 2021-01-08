a7118311f1d0b11ae00c06cb8724bc51b72dbf79
Sujil Kumar Mani <kmsujilkumar@gmail.com>
2021-01-08 19:17:07 +0400

    Verion 1.0
    *Link Creation
    *Controller
    *Middle Ware
    *Routing in web.php

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    echo "This is home page";
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+// Route::get('contact','ContactController@index');
+Route::get('/abcee-fjgkk',[ContactController::class,'index'])->name('con');
+
+
+Route::get('/charity', function () {
+    return view('charity');
+});
